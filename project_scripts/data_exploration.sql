f84e61d0f2bf5e8a466c14c4c2ec89c5cf9887d9
Boluwatito Samuel <samuelgabby04@gmail.com>
2025-11-04 17:34:49 -0300

    Commit

---
+++ project_scripts/data_exploration.sql
@@ -0,0 +1,42 @@
+SELECT *
+FROM category
+
+SELECT *
+FROM customers
+
+SELECT *
+FROM orders
+
+SELECT DISTINCT order_status
+FROM orders
+
+SELECT *
+FROM order_items
+
+SELECT *
+FROM products
+
+
+SELECT *
+FROM payments
+
+SELECT DISTINCT payment_status
+FROM payments
+
+
+SELECT *
+FROM shipping
+
+SELECT DISTINCT shipping_providers
+FROM shipping
+
+
+SELECT DISTINCT delivery_status
+FROM shipping
+
+SELECT *
+FROM inventory
+
+SELECT *
+FROM sellers
+
